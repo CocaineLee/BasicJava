daad40cd914f2f215d109a37df3f8768bb36f8f4
ZacharyLee <1604370138@qq.com>
2019-03-29 15:53:33 +0800

    S1PAPBcomplete

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,19 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String > result=new ArrayList<>();
+        for(String c1Str:collection1){
+            for(List<String> c2Li:collection2)
+                for(String c2Str: c2Li){
+                    if(c1Str.equals(c2Str))
+                        result.add(c1Str);
+                }
+        }
+        return result;
     }
 }
